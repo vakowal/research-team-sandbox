eadb1ed8fb58d5d8dff1c1f5dd79d3286bb9556c
vakowal <ginger.kowal@gmail.com>
2023-04-20 16:02:57 -0400

    filter low/no overshoot scenarios from SR15

--- filter_scenarios.py
+++ filter_scenarios.py
@@ -12,11 +12,30 @@ def replicate_sr15_filter():
 	"""Replicate Andres's filter of SR15 database."""
 	sr15_key_path = os.path.join(
 		_PROJ_DIR, 'IPCC_SR15/sr15_metadata_indicators_r2.0.xlsx')
-	sr15_key = pandas.read_excel(sr15_key_path, sheet_name='meta')
+	scen_key = pandas.read_excel(sr15_key_path, sheet_name='meta')
 
 	sr15_scen_path = os.path.join(
 		_PROJ_DIR, 'IPCC_SR15', 'iamc15_scenario_data_world_r2.0.xlsx')
-	sr15_scen = pandas.read_excel(sr15_scen_path, sheet_name='data')
+	scen_em = pandas.read_excel(sr15_scen_path, sheet_name='data')
+
+	# interquartile range: CO2 from low/no overshoot scenarios
+	# identify SR15 low/no overshoot scenarios
+	sr15_categories = ['Lower 1.5C low overshoot', 'Below 1.5C']
+	filtered_scen = scen_key.loc[
+		(scen_key['category'].isin(sr15_categories)) &
+		(scen_key['Kyoto-GHG|2010 (SAR)'] == 'in range')]
+
+	# summarize CO2 in low/no overshoots, excluding emissions from FLAG,
+	# landfill waste, and fluorinated gases
+	emissions_subs = scen_em.loc[
+		scen_em['Variable'].startswith('Emissions')]
+	print('stop here')
+
+	# add N20 from energy: mean of low/no overshoot scenarios
+	# convert to CO2e using GWP100 from IPCC AR5
+
+	# add CH4 from NZE
+	# convert to CO2e using GWP100 from IPCC AR5
 
 
 def filter_AR6_scenarios():
