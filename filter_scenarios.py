85d3d41e725175b9c08a928404fe92e894c6232c
vakowal <ginger.kowal@gmail.com>
2023-04-24 15:57:12 -0400

    filter AR6 scenarios according to IISD report released Oct 2022

--- filter_scenarios.py
+++ filter_scenarios.py
@@ -49,8 +49,12 @@ def cross_sector_sr15():
 	year_col = [col for col in co2_em if col.startswith('2')] + ['Variable']
 	sr15_25perc = co2_em[year_col].groupby('Variable').quantile(q=0.25)
 	sr15_25perc['scenario_col'] = 'IPCC SR15 (25th percentile)'
+	sr15_50perc = co2_em[year_col].groupby('Variable').quantile(q=0.5)
+	sr15_50perc['scenario_col'] = 'IPCC SR15 (median)'
 	sr15_75perc = co2_em[year_col].groupby('Variable').quantile(q=0.75)
 	sr15_75perc['scenario_col'] = 'IPCC SR15 (75th percentile)'
+	sr15_df = pandas.concat([sr15_25perc, sr15_50perc, sr15_75perc])
+	sr15_df.to_csv("C:/Users/ginger.kowal/Desktop/sr15_filtered_summary.csv")
 
 	# add N2O from energy: mean of low/no overshoot scenarios
 	n2o_var = 'Emissions|N2O|Energy'
@@ -78,9 +82,59 @@ def filter_AR6_scenarios():
 		_PROJ_DIR, 'IPCC_AR6/AR6_Scenarios_Database_World_v1.1.csv')
 	ar6_scen = pandas.read_csv(scen_path)
 
+	# replicate filters of IISD 2022:
+	# C1: low or no overshoot
+	# exclude scenarios exceeding the “medium” feasibility thresholds for the
+	# scaling up of fossil fuel CCS, bioenergy with CCS (BECCS) and
+	# afforestation or reforestation.
+	# should give 26 scenarios.
+	iisd_scen_path = os.path.join(
+		_PROJ_DIR, 'IISD/IISD_2022_filtered_AR6_scenarios.csv')
+	iisd_scen = pandas.read_csv(iisd_scen_path)
+	scen_ids = iisd_scen['Model Scenario']
+
+	ar6_scen['scen_id'] = ar6_scen['Model'] + ' ' + ar6_scen['Scenario']
+	filtered_em = ar6_scen.loc[ar6_scen['scen_id'].isin(scen_ids)]
+
+	# exclude emissions from FLAG, landfill waste, and fluorinated gases
+	# TODO confirm this variable is what we want
+	# not all models contain values for this variable
+	year_col = [col for col in filtered_em if col.startswith('1')] + [
+		col for col in filtered_em if col.startswith('2')]
+	summary_cols = ['scen_id', 'Variable'] + year_col
+	co2_var = 'Emissions|CO2|Energy and Industrial Processes'
+	co2_em = filtered_em.loc[filtered_em['Variable'] == co2_var][summary_cols]
+	co2_em.reset_index(inplace=True)
+	estimate_mod = set(scen_ids).difference(
+		set(filtered_em.loc[filtered_em['Variable'] == co2_var]['scen_id']))
+
+	r_idx = len(co2_em)
+	for sid in estimate_mod:
+		sum_rows = filtered_em.loc[
+			(filtered_em['scen_id'] == sid) &
+			(filtered_em['Variable'].isin(
+				['Emissions|CO2|Energy', 'Emissions|CO2|Industrial Processes']))]
+		sum_vals = sum_rows[year_col].sum()
+		co2_em.loc[r_idx] = sum_vals
+		co2_em.loc[r_idx, 'scen_id'] = sid
+		co2_em.loc[r_idx, 'Variable'] = co2_var
+		r_idx = r_idx + 1
+
+	year_col = year_col + ['Variable']
+	ar6_25perc = co2_em[year_col].groupby('Variable').quantile(q=0.25)
+	ar6_25perc['scenario_col'] = 'IPCC AR6 (25th percentile)'
+	ar6_50perc = co2_em[year_col].groupby('Variable').quantile(q=0.5)
+	ar6_50perc['scenario_col'] = 'IPCC AR6 (50th percentile)'
+	ar6_75perc = co2_em[year_col].groupby('Variable').quantile(q=0.75)
+	ar6_75perc['scenario_col'] = 'IPCC AR6 (75th percentile)'
+	ar6_df = pandas.concat([ar6_25perc, ar6_50perc, ar6_75perc])
+	ar6_df.to_csv("C:/Users/ginger.kowal/Desktop/ar6_filtered_summary.csv")
+
 
 def main():
 	cross_sector_sr15()
+	filter_AR6_scenarios()
+
 
 if __name__ == '__main__':
     main()
